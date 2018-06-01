cd9beaf308a55eda41da0bef24c53720e45d4f1c
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-06-02 00:50:12 +0200

    .

--- tests/auto/service/TestService/main.cpp
+++ tests/auto/service/TestService/main.cpp
@@ -4,5 +4,7 @@
 int main(int argc, char *argv[])
 {
 	TestService service{argc, argv};
+	QCoreApplication::setApplicationName(QStringLiteral("testservice"));
+	QCoreApplication::setApplicationVersion(QStringLiteral("1.0.0"));
 	return service.exec();
 }
