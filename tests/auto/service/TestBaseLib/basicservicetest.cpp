aabed8d88ac1e7030d4e3afe15459a77b14f5c4f
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-10-12 12:02:41 +0200

    .

--- tests/auto/service/TestBaseLib/basicservicetest.cpp
+++ tests/auto/service/TestBaseLib/basicservicetest.cpp
@@ -205,7 +205,7 @@ void BasicServiceTest::testCustomImpl()
 
 void BasicServiceTest::resetSettings(const QVariantHash &args)
 {
-	QSettings config{runtimeDir().absoluteFilePath(QStringLiteral("test.conf")), QSettings::IniFormat};
+	QSettings config{control->runtimeDir().absoluteFilePath(QStringLiteral("test.conf")), QSettings::IniFormat};
 	QVERIFY(config.isWritable());
 	config.clear();
 	config.setValue(QStringLiteral("testval"), true);
