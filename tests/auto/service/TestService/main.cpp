756d106bd00d0c0e0fcec3c039fac10a693134ab
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-11-19 12:58:22 +0100

    ...

--- tests/auto/service/TestService/main.cpp
+++ tests/auto/service/TestService/main.cpp
@@ -3,13 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_CC_MSVC
 	// WORKAROUND for CI bug
+	qputenv("QT_PLUGIN_PATH", QFileInfo{QString::fromUtf8(argv[0])}.dir().absolutePath().toUtf8());
+	qDebug() << "QT_PLUGIN_PATH" << qEnvironmentVariable("QT_PLUGIN_PATH");
+#endif
 	qDebug() << "libraryPaths" << QCoreApplication::libraryPaths();
-	if (QCoreApplication::libraryPaths().isEmpty()) {
-		qputenv("QT_PLUGIN_PATH", QFileInfo{QString::fromUtf8(argv[0])}.dir().absolutePath().toUtf8());
-		qDebug() << "QT_PLUGIN_PATH" << qEnvironmentVariable("QT_PLUGIN_PATH");
-		qDebug() << "libraryPaths" << QCoreApplication::libraryPaths();
-	}
 
 	TestService service{argc, argv};
 	QCoreApplication::setApplicationName(QStringLiteral("testservice"));
