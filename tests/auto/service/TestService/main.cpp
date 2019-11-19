675523042fbf80885ee308bede3f405a24266e05
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-11-19 12:42:12 +0100

    add ci workaround

--- tests/auto/service/TestService/main.cpp
+++ tests/auto/service/TestService/main.cpp
@@ -3,7 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-	qDebug() << QCoreApplication::libraryPaths();
+	// WORKAROUND for CI bug
+	qDebug() << "libraryPaths" << QCoreApplication::libraryPaths();
+	if (QCoreApplication::libraryPaths().isEmpty()) {
+		qputenv("QT_PLUGIN_PATH", QFileInfo{QString::fromUtf8(argv[0])}.dir().absolutePath().toUtf8());
+		qDebug() << "QT_PLUGIN_PATH" << qEnvironmentVariable("QT_PLUGIN_PATH");
+		qDebug() << "libraryPaths" << QCoreApplication::libraryPaths();
+	}
+
 	TestService service{argc, argv};
 	QCoreApplication::setApplicationName(QStringLiteral("testservice"));
 	QCoreApplication::setApplicationVersion(QStringLiteral("1.0.0"));
